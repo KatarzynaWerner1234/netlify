df94d8b5b9cb2135b0946f184a2c02bd921ec3c7
Kasiagit config --global user.name Kasiagit config --global user.name Kasia <kwerner@centrumcyfrowe.pl>
2023-11-12 12:45:58 +0100

    script correction

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const newsletterForm = getElementById('newsletter-form');
+const newsletterForm = document.getElementById('newsletter-form');
 
 newsletterForm.addEventListener('submit', (e) => {
     e.preventDefault();
